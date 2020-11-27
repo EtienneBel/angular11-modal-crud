9516d2dd6913e4ee9f8dbda4e95d3d80bea1fbe6
Etienne <siel.team.group@gmail.com>
2020-11-27 10:08:01 +0000

    last commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CalendarComponent } from './calendar/calendar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { WidgetsComponent } from './widgets/widgets.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'widgets', component: WidgetsComponent },
+  { path: 'gallery', component: GalleryComponent },
+  { path: 'calendar', component:  CalendarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { MenuComponent } from './layouts/menu/menu.component';
+import { SettingsComponent } from './layouts/settings/settings.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { WidgetsComponent } from './widgets/widgets.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { GalleryComponent } from './gallery/gallery.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenuComponent,
+    SettingsComponent,
+    DashboardComponent,
+    WidgetsComponent,
+    CalendarComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule,
